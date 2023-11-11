88229d19b6cbc44921573f5c95bca337031f4045
quangnvo <voquang.usth@gmail.com>
2023-11-11 11:02:59 +0800

    modify ticket card

---
+++ components/StatusDisplay.jsx
@@ -0,0 +1,8 @@
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+
+const StatusDisplay = () => {
+  return <Badge variant="done">Done</Badge>;
+};
+
+export default StatusDisplay;
--- components/TicketCard.tsx
+++ components/TicketCard.tsx
@@ -1,13 +1,19 @@
 import DeleteBlock from "./DeleteBlock"
 import PriorityDisplay from "./PriorityDisplay"
 import ProgressDisplay from "./ProgressDisplay"
+import StatusDisplay from "./StatusDisplay"
 
 const TicketCard = () => {
     return (
-        <div>
-            <DeleteBlock />
-            <PriorityDisplay />
+        <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+            <div className="flex mb-3">
+                <PriorityDisplay />
+                <div className="ml-auto">
+                    <DeleteBlock />
+                </div>
+            </div>
             <ProgressDisplay />
+            <StatusDisplay />
         </div>
     )
 }
