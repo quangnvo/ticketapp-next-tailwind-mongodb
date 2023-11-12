e40514bfb2a10decbc381a4c682a85bbff07051b
quangnvo <voquang.usth@gmail.com>
2023-11-12 22:56:20 +0800

    adding the ticket card styles

--- components/TicketCard.tsx
+++ components/TicketCard.tsx
@@ -16,9 +16,17 @@ const TicketCard = () => {
             <h4>Ticket Title</h4>
             <hr className="h-px border-0 mb-2 bg-gray-300" />
             <p className="whitespace-pre-wrap">This is the ticket description</p>
+            <div className="flex-grow"></div>
 
-            <ProgressDisplay />
-            <StatusDisplay />
+            <div className="flex mt-2">
+                <div className="flex flex-col">
+                    <p className="text-xs my-1">08/31/23 10:43PM</p>
+                    <ProgressDisplay />
+                </div>
+                <div className="ml-auto flex items-end">
+                    <StatusDisplay />
+                </div>
+            </div>
         </div>
     )
 }
